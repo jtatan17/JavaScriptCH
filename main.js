f7f29bc09f8e81686b6e118c049445012d482af0
Sebastian Medina <jtatan2014@gmail.com>
2022-06-13 22:41:51 -0500

    Desafio complementario #2

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
 
-/*
-function calcular (valor, cuotas) {
- let valorcuota = valor / cuotas ;
- let montocuota = "El valor que se debera pagar por " + numerodecuotas + " meses es de " + valorcuota ;
- return montocuota;
-}
-let valordeuda = prompt("Ingrese el valor del credito");
-let numerodecuotas = prompt("Ingrese el numero de cuotas a dividir la deuda");
-alert(calcular(valordeuda, numerodecuotas));
-*/
-let inicio = prompt("Ingrese 1 para calcular las cuotas de su credito o ingrese 2 para terminar")
+const names = ["Posicion 0","Sebastian", "Juan","Daniel","Jorge","Ricardo","Felipe","Juan","Alejandro","Camilo","Gabriel"];
+
+let inicio = prompt("En una carrera participaron Jorge, Alejandro, Ricardo, Sebastian, Daniel, Felipe, Juan, Camilo, Gabriel y Juan. Por medio de los alerts puedes saber en que posicion llego cada uno. Ingrese 1 para saber en que posicion llego alguno de los participantes o ingrese 2 para terminar")
 while (inicio != 2 ) {
-    function calcular (valor, cuotas) {
-        let valorcuota = valor / cuotas ;
-        let montocuota = "El valor que se debera pagar por " + numerodecuotas + " meses es de " + valorcuota + " $";
-        return montocuota;
-       }
-       let valordeuda = prompt("Ingrese el valor del credito");
-       let numerodecuotas = prompt("Ingrese el numero de cuotas a dividir la deuda");
-       alert(calcular(valordeuda, numerodecuotas));
-    inicio = prompt("Ingrese 1 para calcular las cuotas de su credito o ingrese 2 para terminar")
+    function localizarnombre (nombre) {
+        const namesLocation = names.indexOf(nombre);
+        return namesLocation;
+    }
+    let pregunta = prompt("Lista de nombres: Jorge, Alejandro, Ricardo, Sebastian, Daniel, Felipe, Juan, Camilo, Gabriel y Juan.Ingrese el nombe de la persona de la cual quiere saber en que posicion llego(escribir exactamente el nombre)");
+    alert(pregunta + " llego en el lugar numero " + (localizarnombre(pregunta)));
+    inicio = prompt("Ingrese 1 para continuar averiguando posiciones o ingrese 2 para terminar")
 }
+
