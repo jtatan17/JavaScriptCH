9d61a7c56d0da6abf21c7394d065a1e7127a1f89
Sebastian Medina <jtatan2014@gmail.com>
2022-06-16 23:23:52 -0500

    1ra preentrega

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 
-const names = ["Posicion 0","Sebastian", "Juan","Daniel","Jorge","Ricardo","Felipe","Juan","Alejandro","Camilo","Gabriel"];
-
-let inicio = prompt("En una carrera participaron Jorge, Alejandro, Ricardo, Sebastian, Daniel, Felipe, Juan, Camilo, Gabriel y Juan. Por medio de los alerts puedes saber en que posicion llego cada uno. Ingrese 1 para saber en que posicion llego alguno de los participantes o ingrese 2 para terminar")
+let inicio = prompt("Simulador de credito\n Para solicitar un credito escriba 1 para terminar precione 2");
 while (inicio != 2 ) {
-    function localizarnombre (nombre) {
-        const namesLocation = names.indexOf(nombre);
-        return namesLocation;
+    let credit = prompt("Tenga en cuenta que si el credito es menor a $1500 tendra una tasa de interes del 5%, mientras que si el credito es mayor a $1500 tendra una tasa de interes del 3.5%.\n Ingrese el valor del credito a pedir entre $100 y $10000");
+
+    let numeroCuotas = prompt("Ingrese el numero de cuotas en las que quiere dividir el credito\n (Sugerencia, dividir el credito en 12, 36 o 72 cuotas)");
+    function simuladorMenor (valor, cuotas){
+        let valorCuota = valor / cuotas ;
+        let interest = valorCuota * 0.5;
+        let result = alert("Tu credito de $" + credit + " sera divido en " + numeroCuotas + " cuotas. Cada cuota tendra un valor de $" + valorCuota + " mas $" + interest + ", equivalente al 5% de intereses.")
+        return result;
     }
-    let pregunta = prompt("Lista de nombres: Jorge, Alejandro, Ricardo, Sebastian, Daniel, Felipe, Juan, Camilo, Gabriel y Juan.Ingrese el nombe de la persona de la cual quiere saber en que posicion llego(escribir exactamente el nombre)");
-    alert(pregunta + " llego en el lugar numero " + (localizarnombre(pregunta)));
-    inicio = prompt("Ingrese 1 para continuar averiguando posiciones o ingrese 2 para terminar")
+    function simuladorMayor (valor, cuotas){
+        let valorCuota = valor / cuotas ;
+        let interest = valorCuota * 0.35;
+        let result = alert("Tu credito de $" + credit + " sera divido en " + numeroCuotas + " cuotas. Cada cuota tendra un valor de $" + valorCuota + " mas $" + interest + ", equivalente al 3.5% de intereses.")
+        return result;
+    }
+    
+    if (credit < 1500 ) {
+        simuladorMenor(credit, numeroCuotas);
+    } else {
+        simuladorMayor(credit, numeroCuotas);
+    }
+    
+    inicio = prompt("Simulador de credito\n Para solicitar otro credito escriba 1 para terminar precione 2");
 }
+
 
